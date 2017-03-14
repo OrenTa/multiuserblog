04212204428e1e1b89d0d6836a917fde94b1fe91
Oren Tayar <oren.tayar@gmail.com>
2017-03-14 09:07:52 +0200

    addcomment better verification of logged user

--- handlers/addcomment.py
+++ handlers/addcomment.py
@@ -5,14 +5,20 @@ from google.appengine.ext import db
 
 class AddComment(Handler):
     def post(self):
-        user = self.request.cookies.get('user',-1)     
-        (cookie_uid,cookie_hash) = user.split("|")
-        dbuser = User.get_by_id(int(cookie_uid))
-        dbpost = Post.get_by_id(int(self.request.get('postid')))
-        if dbpost:
-            comment = self.request.get('comment')
-            c = Comment(user=dbuser, post=dbpost, content=comment)
-            c.put()
-            self.redirect("/"+str(dbpost.key().id()))
-        else:
-            self.redirect("/message/9") # a "something went wrong" message
+        user = self.request.cookies.get('user',-1)
+        if user==(-1): 
+            self.redirect("/message/1")
+        else: 
+            (cookie_uid,cookie_hash) = user.split("|")
+            dbuser = User.get_by_id(int(cookie_uid))
+            if dbuser and (dbuser.HashedPassword==cookie_hash):
+                dbpost = Post.get_by_id(int(self.request.get('postid')))
+                if dbpost:
+                    comment = self.request.get('comment')
+                    c = Comment(user=dbuser, post=dbpost, content=comment)
+                    c.put()
+                    self.redirect("/"+str(dbpost.key().id()))
+                else:
+                    self.redirect("/message/9") # a "something went wrong" message
+            else:
+                self.redirect("/message/1")
